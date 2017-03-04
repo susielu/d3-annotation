ae0a7e7c1c9b86d072a48e9a80ace167f0f04953
Susie Lu <slu@netflix.com>
2017-03-04 13:53:51 -0800

    fix npm issues

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import annotation from './src/Adapter-d3'
-import types from './src/Types-d3'
+import { Type, d3Label, d3Callout, d3CalloutCurve, d3CalloutElbow, d3CalloutCircle, 
+ d3CalloutRect, d3XYThreshold, d3Badge, customType } from './src/Types-d3'
+
+export {
+  annotation,
+  Type as annotationTypeBase ,
+  d3Label as annotationLabel ,
+  d3Callout as annotationCallout ,
+  d3CalloutCurve as annotationCalloutCurve ,
+  d3CalloutElbow as annotationCalloutElbow ,
+  d3CalloutCircle as annotationCalloutCircle ,
+  d3CalloutRect as annotationCalloutRect ,
+  d3XYThreshold as annotationXYThreshold ,
+  d3Badge as annotationBadge ,
+  customType as annotationCustomType 
+}
 
 export default {
   annotation,
-  annotationCallout : types.d3Callout,
-  annotationCalloutCircle : types.d3CalloutCircle,
-  annotationXYThreshold : types.d3XYThreshold,
-  annotationLabel: types.d3Label
+  annotationTypeBase : Type,
+  annotationLabel : d3Label,
+  annotationCallout : d3Callout,
+  annotationCalloutCurve : d3CalloutCurve,
+  annotationCalloutElbow : d3CalloutElbow,
+  annotationCalloutCircle : d3CalloutCircle,
+  annotationCalloutRect : d3CalloutRect,
+  annotationXYThreshold : d3XYThreshold,
+  annotationBadge : d3Badge,
+  annotationCustomType : customType
 }
